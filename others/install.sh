3b6975d7d750856428dbd115c75a40a0a9948a69
alforslu <alex@forslund.dev>
2025-02-04 22:44:26 +0100

    Add install script for pacman + fonts

---
+++ others/install.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+set -e
+
+# Replace pacman config (backup existing config)
+[ -f /etc/pacman.conf ] && cp /etc/pacman.conf /etc/pacman.conf.bak
+cp "$(dirname "$0")/pacman.conf" /etc/pacman.conf
+
+# Update package database
+pacman -Syu --noconfirm
+
+# Install fonts
+pacman -S --noconfirm \
+  ttf-noto-nerd \
+  noto-fonts-emoji \
+  noto-fonts-cjk \
+  noto-fonts \
+  ttf-jetbrains-mono-nerd \
+  ttf-jetbrains-mono
+
+echo "Installation complete."
