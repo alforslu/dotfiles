719a046d9451bede6165e73e9f1027d92962929c
alforslu <alex@forslund.dev>
2025-04-23 14:02:55 +0200

    Update configs

--- fish/config.fish
+++ fish/config.fish
@@ -6,3 +6,6 @@ set -Ux EDITOR nvim
 keychain --quiet --eval ~/.ssh/id_ed25519 | source
 starship init fish | source
 zoxide init fish --cmd cd | source
+
+set -x CPATH /usr/include
+set -x LIBRARY_PATH /usr/lib
